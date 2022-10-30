d46fc275f9f18ed4690954a3031e1c1649a4b18d
NakamuraTakashi <nakamura.t.av@m.titech.ac.jp>
2022-10-30 23:39:01 +0900

    Added: coral_exp_T04 project

--- Projects/coral_exp_T04/run.sh
+++ Projects/coral_exp_T04/run.sh
@@ -9,6 +9,15 @@ rm *.mod
 #
 mkdir -p output
 #
-#./ecosys_test.exe < coral_01.in
-./ecosys_test.exe < coral_exp_T04.in
+./ecosys_test.exe < coral_pre_2w.in
+#./ecosys_test.exe < coral_main_L0000_T28.in
+#./ecosys_test.exe < coral_main_L0000_T32.in
+#./ecosys_test.exe < coral_main_L0100_T28.in
+#./ecosys_test.exe < coral_main_L0100_T32.in
+#./ecosys_test.exe < coral_main_L0250_T28.in
+#./ecosys_test.exe < coral_main_L0250_T32.in
+#./ecosys_test.exe < coral_main_L0500_T28.in
+#./ecosys_test.exe < coral_main_L0500_T32.in
+#./ecosys_test.exe < coral_main_L1000_T28.in
+#./ecosys_test.exe < coral_main_L1000_T32.in
 #
---
+++ Projects/coral_exp_T04/run_main.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#./ecosys_test.exe < coral_pre_2w.in
+./ecosys_test.exe < coral_main_L0000_T28.in
+./ecosys_test.exe < coral_main_L0000_T32.in
+./ecosys_test.exe < coral_main_L0100_T28.in
+./ecosys_test.exe < coral_main_L0100_T32.in
+./ecosys_test.exe < coral_main_L0250_T28.in
+./ecosys_test.exe < coral_main_L0250_T32.in
+./ecosys_test.exe < coral_main_L0500_T28.in
+./ecosys_test.exe < coral_main_L0500_T32.in
+./ecosys_test.exe < coral_main_L1000_T28.in
+./ecosys_test.exe < coral_main_L1000_T32.in
+#
