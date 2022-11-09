cb005f49dd3d98a2ea4413f4cc48a107579e853a
Takashi NAKAMURA <nakamura.t.av@m.titech.ac.jp>
2022-11-10 08:29:33 +0900

    Modified: mod_coral.F

--- Projects/coral_exp_T04/run_main.sh
+++ Projects/coral_exp_T04/run_main.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 #
+rm *.exe
+#
+SRC_DIR=../../src
+INCLUDE="-I${PWD}"
+gfortran -fbounds-check -ffree-form ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys.F ${SRC_DIR}/mod_reef_ecosys.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/usr/include -L/usr/lib -lnetcdff -o ecosys_test.exe
+rm *.mod
+#
+mkdir -p output
+#
 #./ecosys_test.exe < coral_pre_2w.in
 ./ecosys_test.exe < coral_main_L0000_T28.in
 ./ecosys_test.exe < coral_main_L0000_T32.in
